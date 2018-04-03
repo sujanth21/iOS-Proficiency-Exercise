365929b7674eca68680899408875babec099628b
Sujanth <sujanth1987@gmail.com>
2018-04-03 14:05:41 +1000

    Created collection view cells loaded with static emoji values

--- iOS Proficiency Exercise/ViewController.swift
+++ iOS Proficiency Exercise/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var collectionData = ["1 🐶", "2 🐰", "3 🦊", "4 🦁", "5 🐸", "6 🐮", "7 🐔", "8 🐷", "9 🐒", "10 🦉", "11 🐢", "12 🦖"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +23,27 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return collectionData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        
+        if let label = cell.viewWithTag(100) as? UILabel {
+            label.text = collectionData[indexPath.row]
+        }
+        
+        return cell
+    }
+    
+    
+    
 }
 
